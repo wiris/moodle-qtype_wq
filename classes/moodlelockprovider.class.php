d59cf7d6e7568de26c747f3d22ed1a9123d258d8
AndresMelo <Andres.Torres@blackboard.com>
2016-12-05 15:01:56 -0500

    Modification in absolute path

--- classes/moodlelockprovider.class.php
+++ classes/moodlelockprovider.class.php
@@ -26,7 +26,7 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-require_once($CFG->dirroot . '/question/type/classes/moodlelock.php');
+require_once($CFG->dirroot . '/question/type/wq/classes/moodlelock.php');
 
 class moodlelockprovider {
 
