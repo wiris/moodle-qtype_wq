8bc0c22fc652b07792b894fb87eb8c99177b016d
Manuel <manuel@wiris.com>
2018-03-11 23:50:03 +0100

    2018031100 development version

--- version.php
+++ version.php
@@ -16,7 +16,7 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version = 2018031000;
+$plugin->version = 2018031100;
 $plugin->requires = 2011120500; // Moodle 2.2.
 
 $plugin->maturity = MATURITY_BETA;
