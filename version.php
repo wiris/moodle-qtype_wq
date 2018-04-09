5fa727d570efd1f60a6a87b48c81d6a402a39989
Manuel <manuel@wiris.com>
2018-04-09 09:01:35 +0200

    2018040900 Stable version

--- version.php
+++ version.php
@@ -18,8 +18,8 @@ defined('MOODLE_INTERNAL') || die();
 
 $plugin->version = 2018040900;
 $plugin->requires = 2011120500; // Moodle 2.2.
-
-$plugin->maturity = MATURITY_BETA;
+$plugin->release = '3.63.0.1037';
+$plugin->maturity = MATURITY_STABLE;
 $plugin->component = 'qtype_wq';
 $plugin->dependencies = array (
      'filter_wiris' => ANY_VERSION
