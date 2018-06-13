357fc24b689b7cba41025c2d0c554791b1537abf
Manuel Cagigas <manuel@wiris.com>
2018-06-13 15:54:36 +0200

    3.65.2 stable version

--- version.php
+++ version.php
@@ -16,9 +16,9 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version = 2018060600;
+$plugin->version = 2018061300;
 $plugin->requires = 2011120500; // Moodle 2.2.
-$plugin->release = '3.65.1.1040';
+$plugin->release = '3.65.2.1041';
 $plugin->maturity = MATURITY_STABLE;
 $plugin->component = 'qtype_wq';
 $plugin->dependencies = array (
