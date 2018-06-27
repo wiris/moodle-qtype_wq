383f6410cade9196587dde62327df95254d1c0b8
Jenkins <jenkins@wiris.com>
2018-06-28 00:31:18 +0200

    2018062800 development version

--- version.php
+++ version.php
@@ -16,7 +16,7 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version = 2018062600;
+$plugin->version = 2018062800;
 $plugin->requires = 2011120500; // Moodle 2.2.
 
 $plugin->maturity = MATURITY_BETA;
