665d2b92d9c0085eee8039f68b91a2739391c559
Jenkins <jenkins@wiris.com>
2018-12-12 14:15:39 +0100

    2018121200 Stable version

--- version.php
+++ version.php
@@ -18,8 +18,8 @@ defined('MOODLE_INTERNAL') || die();
 
 $plugin->version = 2018121200;
 $plugin->requires = 2011120500; // Moodle 2.2.
-
-$plugin->maturity = MATURITY_BETA;
+$plugin->release = '3.71.0.1053';
+$plugin->maturity = MATURITY_STABLE;
 $plugin->component = 'qtype_wq';
 $plugin->dependencies = array (
      'filter_wiris' => ANY_VERSION
