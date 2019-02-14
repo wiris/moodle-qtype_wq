a5dcc962879d9f63a91e9bd31254742fe305514c
Jenkins <jenkins@wiris.com>
2019-02-14 17:25:04 +0100

    2019021300 development version

--- version.php
+++ version.php
@@ -16,10 +16,10 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version = 2019021200;
+$plugin->version = 2019021300;
 $plugin->requires = 2011120500; // Moodle 2.2.
-$plugin->release = '3.73.0.1054';
-$plugin->maturity = MATURITY_STABLE;
+
+$plugin->maturity = MATURITY_BETA;
 $plugin->component = 'qtype_wq';
 $plugin->dependencies = array (
      'filter_wiris' => ANY_VERSION
