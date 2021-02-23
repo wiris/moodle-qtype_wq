280ffd85672d8d7dd6d589d59acb4cd7729a9f9a
Pol Torrent <pol.torrent@wiris.com>
2021-02-23 15:37:22 +0100

    v4.2.4

--- version.php
+++ version.php
@@ -16,9 +16,9 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version = 2021021200;
+$plugin->version = 2021022300;
 $plugin->requires = 2011120500; // Moodle 2.2.
-$plugin->release = '4.2.3';
+$plugin->release = '4.2.4';
 $plugin->maturity = MATURITY_RC;
 $plugin->component = 'qtype_wq';
 $plugin->dependencies = array (
