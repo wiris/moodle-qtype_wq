de3747ec2f2f19a86dcbf071d8af0cdcf04ca7a9
pol-at-wiris <66125018+pol-at-wiris@users.noreply.github.com>
2021-05-06 17:07:01 +0200

    Update version.php

--- version.php
+++ version.php
@@ -16,7 +16,7 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version = 2021042100;
+$plugin->version = 2021050700;
 $plugin->requires = 2011120500; // Moodle 2.2.
 $plugin->release = '4.2.6';
 $plugin->maturity = MATURITY_RC;
