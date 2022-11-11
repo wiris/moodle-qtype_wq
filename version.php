9780998d60324cf93bb19fdbf9ed8bb3611e723d
Pol Torrent i Soler <66125018+ptorrent-at-wiris@users.noreply.github.com>
2022-11-11 16:19:50 +0100

    chore: bump version number to 4.5.3

--- version.php
+++ version.php
@@ -16,9 +16,9 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version = 2022090700;
+$plugin->version = 2022111100;
 $plugin->requires = 2015111600; // Moodle 3.0.
-$plugin->release = '4.5.2';
+$plugin->release = '4.5.3';
 $plugin->maturity = MATURITY_STABLE;
 $plugin->component = 'qtype_wq';
 $plugin->dependencies = array (
