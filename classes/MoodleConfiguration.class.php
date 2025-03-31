576c40cd32c19f2036afd45dfff3933880e91306
Pol Torrent i Soler <pol.torrent@wiris.com>
2025-03-31 16:40:50 +0200

    chore: remove unused import

--- classes/MoodleConfiguration.class.php
+++ classes/MoodleConfiguration.class.php
@@ -14,8 +14,6 @@
 // You should have received a copy of the GNU General Public License
 // along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
 
-use core_reportbuilder\external\columns\sort\get;
-
 defined('MOODLE_INTERNAL') || die();
 
 require_once(dirname(__FILE__) . '/../quizzes/lib/com/wiris/quizzes/api/Configuration.interface.php');
